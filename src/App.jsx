e965fc7a617523d2eea08ade46993028411db590
Tami Lin <timasvx@gmail.com>
2026-04-20 22:10:33 +0500

    added storage and button

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,25 @@ import './App.css'
 import Hero from './components/Hero'
 import Feature from './components/Feature'
 import Footer from './components/Footer'
+import { useEffect } from 'react'
 
 const featureData = [{id: 1, title: 'Fast', text: 'All flying'}, {id: 2, title: 'Reliably', text: 'Git with ctrl'}];
 
 function App() {
-  const [email, setEmail] = useState('')
+  const [email, setEmail] = useState(localStorage.getItem('myEmail') || '');
+
+  useEffect(() => {
+    localStorage.setItem('myEmail', email);
+  }, [email]);
+
+  const handleSubscride = () => {
+    alert(`Sucrisbilly! We get new in: ${email}`);
+
+    setEmail('');
+
+    localStorage.removeItem('myEmail');
+  };
+    
 
   return (
     <>
@@ -18,6 +32,7 @@ function App() {
         </div>
         <section className='follow'>
           <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <button onClick={handleSubscride}>Subscride</button>
           <p>Your input: {email}</p>
           {email.length > 0 && email.length < 5 && <p style={{ color: 'red'}}>Few sumbols</p>}
         </section>
