32441781f3a2012d8555bbf229faa583adaa361d
Tami Lin <timasvx@gmail.com>
2026-04-23 00:14:40 +0500

    added motion in User and Home

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,12 @@
+import { motion } from "framer-motion"
+
 const Hero = (props) => {
     return (
         <section>
-            <h1>{props.title}</h1>
+            <motion.h1
+            initial={{ opacity: 0, y: -50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8 }}>{props.title}</motion.h1>
             <p>{props.text}</p>
             <button>{props.buttonText}</button>
         </section>
--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,11 +1,16 @@
 import './UserCard.css'
+import { motion } from 'framer-motion'
 
 const UserCard = ({user}) => {
     return (
-        <div className="user-card">
+        <motion.div className="user-card"
+            initial={{ opacity: 0, scale: 0.5 }}
+            whileInView={{ opacity: 1, scale: 1 }}
+            viewport={{ once: true }}
+            transition={{ duration: 0.8 }}>
             <div className="user-name">{user.name.first} {user.name.last}</div>
             <img src={user.picture.medium} alt="user" />
-        </div>
+        </motion.div>
     )   
 }
 
